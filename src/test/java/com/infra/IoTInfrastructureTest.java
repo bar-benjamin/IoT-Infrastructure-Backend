8599d2383596362410db545c613ffc2304923c43
bar-benjamin <bar.benjamin123@gmail.com>
2023-07-03 03:28:18 +0300

    Added JUnit4 Support

--- src/test/java/com/infra/IoTInfrastructureTest.java
+++ src/test/java/com/infra/IoTInfrastructureTest.java
@@ -1,24 +1,25 @@
 package com.infra;
 
-import io.dropwizard.testing.junit.DropwizardAppRule;
-import org.junit.ClassRule;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import com.infra.representations.Address;
+import com.infra.representations.Company;
+import com.infra.representations.Contact;
+import com.infra.representations.PaymentInfo;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
-import com.infra.representations.*;
-import static org.junit.Assert.assertEquals;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
-// TODO: Note - App must be running before running tests
+import static org.junit.Assert.assertEquals;
 
 public class IoTInfrastructureTest {
-    // RULE is used to start and stop the application during the test
+    // RULE is used to start and stop the application before and after tests
     @ClassRule
     public static final DropwizardAppRule<IoTInfrastructureConfiguration> RULE = new DropwizardAppRule<>(IoTInfrastructureApplication.class, "config.yml");
 
@@ -28,26 +29,28 @@ public class IoTInfrastructureTest {
 
     private static final String COMPANY_NAME = "BarGPT";
 
-    @BeforeAll
+    private static final int COMPANY_ID = 1;
+
+
+    @BeforeClass
     public static void setup() {
         companyResource = Client.create().resource("http://localhost:8080/GIOTI/companies");
         company = new Company(COMPANY_NAME,
                 new Address(1, "Israel", "Tel-Aviv", "Gordon", 630105),
                 new ArrayList<>(Arrays.asList(new Contact(1, "bar corporation", "BarGPT@gmail.com", "055-1112233", 1))),
                 new ArrayList<>(Arrays.asList(new PaymentInfo(1, "5326458012345678", "03/25", "111"))));
+        companyResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, company);
     }
 
     @Test
     public void testRegisterCompanyConflict() {
-        ClientResponse existResponse = companyResource.type(MediaType.APPLICATION_JSON)
-                .post(ClientResponse.class, company);
-        assertEquals(Response.Status.CONFLICT.getStatusCode(), existResponse.getStatus());
+        ClientResponse response = companyResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, company);
+        assertEquals(Response.Status.CONFLICT.getStatusCode(), response.getStatus());
     }
 
     @Test
     public void testGetCompany() {
-        int companyId = 1;
-        ClientResponse response = companyResource.path(String.valueOf(companyId))
+        ClientResponse response = companyResource.path(String.valueOf(COMPANY_ID))
                 .accept(MediaType.APPLICATION_JSON)
                 .get(ClientResponse.class);
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
